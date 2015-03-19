aea57489038528d2121599e8321c42be7e949197
williamscs <cwilliams88@gmail.com>
2015-03-18 21:41:03 -0500

    It was an overflow error!

--- Project Euler/project7.cpp
+++ Project Euler/project7.cpp
@@ -1,6 +1,7 @@
 #include "project7.h"
 #include <stdio.h>
 #include <cmath>
+#include <fstream>
 
 project7::project7(void)
 {
@@ -10,15 +11,16 @@ project7::project7(void)
 project7::~project7(void)
 {
 }
-std::vector<bool> project7::sieve_of_eratosthenes(unsigned cap){
-	std::vector<bool> arr(cap);
+std::vector<bool> project7::sieve_of_eratosthenes(float float_cap){
+	unsigned cap = (unsigned)float_cap + 0.5;
+	std::vector<bool> arr((unsigned)cap);
 	for(unsigned i = 0; i < cap; i++){
 		arr[i] = true;
 	}
-	unsigned prime = 2;
-	for(unsigned i = 2; i < cap; i++){
+	
+	for(unsigned long long i = 2; i < cap; i++){
 		if(arr[i] == true){
-			for(long unsigned j = i*i; j < cap; j = j + i){
+			for(unsigned long long j = i*i; j < cap; j = j + i){
 				arr[j] = false;
 			}
 		}
@@ -26,24 +28,25 @@ std::vector<bool> project7::sieve_of_eratosthenes(unsigned cap){
 	return arr;
 }
 
-unsigned project7::find_cap(unsigned n){
-	return 2*n*logl(n);
+float project7::find_cap(unsigned n){
+	return 2*n*(float)log(n);
 }
 
 unsigned project7::find_nth_prime(unsigned n){
-	unsigned cap = project7::find_cap(n);
+	float cap = project7::find_cap(n);
 	std::vector<bool> arr = project7::sieve_of_eratosthenes(cap);
 	unsigned count = 0;
 	unsigned i = 0;
 	for(i = 2; count < n; i++){
-		if(arr[i] == true)
+		if (arr[i] == true){
 			count++;
+		}
 	}
 	return --i;
 }
 
 void project7::runProject(){
-	unsigned n = 8138;
+	unsigned n = 10001;
 
 	printf("Result is: %u", project7::find_nth_prime(n));
 }
--- Project Euler/project7.h
+++ Project Euler/project7.h
@@ -5,8 +5,8 @@ class project7
 public:
 	project7(void);
 	~project7(void);
-	static std::vector<bool> sieve_of_eratosthenes(unsigned);
-	static unsigned find_cap(unsigned);
+	static std::vector<bool> sieve_of_eratosthenes(float);
+	static float find_cap(unsigned);
 	static unsigned find_nth_prime(unsigned);
 	static void runProject();
 };
